7c5b848d366ab0876f92128732af18acb0b52ddd
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-04-08 17:08:11 -0300

    feat: create valid cpf and valid age minor

---
+++ build/src/utils/age.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.calculate = (birthdate) => {
+    const date = new Date(birthdate);
+    const year = date.getFullYear();
+    const yearNow = new Date().getFullYear();
+    const age = yearNow - year;
+    const minor = age < 18;
+    return minor;
+};
---
+++ build/src/utils/cpfValid.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.valid = function validCPF(cpf) {
+    const Cpf = cpf.replace(/[.-]/g, '');
+    if (Cpf.length !== 11 ||
+        Cpf === '00000000000' ||
+        Cpf === '11111111111' ||
+        Cpf === '22222222222' ||
+        Cpf === '33333333333' ||
+        Cpf === '44444444444' ||
+        Cpf === '55555555555' ||
+        Cpf === '66666666666' ||
+        Cpf === '77777777777' ||
+        Cpf === '88888888888' ||
+        Cpf === '99999999999')
+        return 'Invalid CPF';
+    let add = 0;
+    let i;
+    let rev;
+    for (i = 0; i < 9; i++)
+        add += parseInt(Cpf.charAt(i), 10) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev === 10 || rev === 11)
+        rev = 0;
+    if (rev !== parseInt(Cpf.charAt(9), 10))
+        return 'Invalid CPF';
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(Cpf.charAt(i), 10) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev === 10 || rev === 11)
+        rev = 0;
+    if (rev !== parseInt(Cpf.charAt(10), 10))
+        return 'Invalid CPF';
+    return null;
+};
