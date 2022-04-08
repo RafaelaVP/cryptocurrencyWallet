80083b7e63a7d2363291858fbc049791b527a196
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-04-08 17:12:11 -0300

    feat: add getbyAddress from add valid cpf and age in create

--- src/services/wallets/walletService.js
+++ src/services/wallets/walletService.js
@@ -1,8 +1,14 @@
+const { ApolloError } = require('apollo-server-express');
 const models = require('../../models');
-
+const validCpf = require('../../utils/cpfValid');
+const validAge = require('../../utils/age')
 const createWallet = async (data) => {
+     const isCPFinvalid = validCpf.valid(data.cpf);
+    if(isCPFinvalid) throw new ApolloError("cpf invalido");
+    const minorAge = validAge.calculate(data.birthdate);
+    if(minorAge) throw new ApolloError("menor de idade");
     const result = await models.wallet.create(data);
-    return true;
+    return result;
 }
 
 const getAllWallet = async (search) => {
@@ -10,5 +16,16 @@ const getAllWallet = async (search) => {
     return result;
 }
 
+const updateWallet = async(data) => {
+    await models.wallet.update(data, {where: {address:data.address}});
+    return true;
+}
+const deleteWallet = async (address) => {
+    return await models.wallet.destroy({where: {address}})
+}
+const getByAddressWallet = async (address) => {
+    const result = await models.wallet.findOne({where: {address}})
+    return result
+}
 
-module.exports = {createWallet, getAllWallet}
+module.exports = {createWallet, getAllWallet, updateWallet, deleteWallet, getByAddressWallet}
