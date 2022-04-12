63204cbf0acbd85653d20d2b3b841a060e87198e
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-04-12 13:40:27 -0300

    feat: create model coin and created relation ship

---
+++ src/models/coin.js
@@ -0,0 +1,35 @@
+"use strict";
+const Sequelize = require('sequelize');
+module.exports = function (sequelize, DataTypes) {
+    const Coin = sequelize.define('coin', {
+        id: {
+            type: Sequelize.UUID,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4
+        },
+        coin: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        fullname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        amont: {
+            type: Sequelize.DECIMAL,
+            allowNull: false
+        },
+        walletAddress: {
+            type: Sequelize.UUID,
+            defaultValue:Sequelize.UUIDV4
+        }
+    }, { tableName: 'coins' });
+    Coin.associate = function(models) {
+        Coin.belongsTo(models.wallet, {foreignKey: 'walletAddress', as: 'wallets'})
+      }
+
+
+    return Coin;
+};
